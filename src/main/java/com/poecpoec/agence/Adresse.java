a5ef3edc3a3847324e6dc3e97b3e2cf41eb158ee
ArnaudTLJ <arnaud.tlj@laposte.net>
2017-07-07 15:16:45 +0200

    IT#1 Création + tests dans main de la classe Adresse

---
+++ src/main/java/com/poecpoec/agence/Adresse.java
@@ -0,0 +1,64 @@
+package com.poecpoec.agence;
+
+public class Adresse {
+    
+    // Attributs
+    private String adresse;
+    private String codePostal;
+    private String ville;
+    private String pays;
+    private int id;
+    
+    // Constructeur
+    public Adresse() {
+        
+    }
+
+    // Getters(assesseurs) et Setters(mutateurs)
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public String getPays() {
+        return pays;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Adresse ID " + id + " : " + adresse + " " + codePostal + " - " + ville + " [" + pays + "]";
+    }
+    
+    
+}
--- src/main/java/com/poecpoec/agence/Main.java
+++ src/main/java/com/poecpoec/agence/Main.java
@@ -3,11 +3,6 @@
  */
 package com.poecpoec.agence;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,47 +17,57 @@ public class Main
      */
     public static void main(String[] args)
     {
-        /*
-         * Tests unitaires itération #1
-         */
-        System.out.println("Test it#1");
-        Aeroport cdg = new Aeroport("CDG");
-        Ville paris = new Ville("Paris", "France");
-        Ville londres = new Ville("Londres", "UK");
-        System.out.println(cdg);
-        System.out.println(paris);
-
-        /*
-         * UT it#2
-         */
-        System.out.println("Test it#2");
-        cdg.ajouterVille(paris);
-        cdg.ajouterVille(londres);
-        System.out.println(cdg);
-
-        /*
-         * UT it#3
-         */
-        System.out.println("Test it#3");
-        System.out.println(paris);
-
-        /*
-         * UT it#4 : test de la connexion à la BDD
-         */
-        // déclaration des variables
-        List<Aeroport> aeroports = new ArrayList<>();
-
-
-        // j'ai besoin du DAO, alors je l'instancie
-        AeroportDao aeroportDao = new AeroportDao();
-        // j'appelle la méthode pour récupérer tous les objets de classe
-        // Aéroport de la BDD à l'aide du DAO Aéroport
-        aeroports = aeroportDao.findAll();
+//        /*
+//         * Tests unitaires itération #1
+//         */
+//        System.out.println("Test it#1");
+//        Aeroport cdg = new Aeroport("CDG");
+//        Ville paris = new Ville("Paris", "France");
+//        Ville londres = new Ville("Londres", "UK");
+//        System.out.println(cdg);
+//        System.out.println(paris);
+//
+//        /*
+//         * UT it#2
+//         */
+//        System.out.println("Test it#2");
+//        cdg.ajouterVille(paris);
+//        cdg.ajouterVille(londres);
+//        System.out.println(cdg);
+//
+//        /*
+//         * UT it#3
+//         */
+//        System.out.println("Test it#3");
+//        System.out.println(paris);
+//
+//        /*
+//         * UT it#4 : test de la connexion à la BDD
+//         */
+//        // déclaration des variables
+//        List<Aeroport> aeroports = new ArrayList<>();
+//
+//
+//        // j'ai besoin du DAO, alors je l'instancie
+//        AeroportDao aeroportDao = new AeroportDao();
+//        // j'appelle la méthode pour récupérer tous les objets de classe
+//        // Aéroport de la BDD à l'aide du DAO Aéroport
+//        aeroports = aeroportDao.findAll();
+//        
+//
+//        // j'affiche mes aéroports
+//        System.out.println(aeroports);
         
-
-        // j'affiche mes aéroports
-        System.out.println(aeroports);
-
+        // TEAM MMA
+        
+        //IT#1 Création classe Adresse
+        Adresse adresse = new Adresse();
+        adresse.setId(1);
+        adresse.setAdresse("15bis, Allée James Watt");
+        adresse.setCodePostal("33700");
+        adresse.setVille("Mérignac");
+        adresse.setPays("France");
+        System.out.println(adresse);
     }
 
 }
